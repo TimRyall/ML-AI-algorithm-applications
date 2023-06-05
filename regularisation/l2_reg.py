db32457a60b836723824481bc21971d796f66528
Tim <78301985+TimRyall@users.noreply.github.com>
2023-06-05 22:08:00 +1000

    Update l2_reg.py

--- regularisation/l2_reg.py
+++ regularisation/l2_reg.py
@@ -12,7 +12,7 @@ from sklearn.linear_model import Ridge
 #####################################################################
 lambda_param = 0.01
 
-data = pd.read_csv("4.1-linear-regression/pokemonregr.csv")
+data = pd.read_csv("linear-regression/pokemonregr.csv")
 data = data.sample(frac=1) # shuffle data
 data = data.fillna(0)
 
